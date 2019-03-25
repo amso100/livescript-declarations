badd6ebb8a95b97bd01856d45d4aaac421e0225c
ampc <amit.so100@gmail.com>
2019-03-25 11:14:00 +0200

    renamed main

--- main.rb
+++ main.rb
@@ -1,6 +1,155 @@
+# This will be the main "declarations" program, which will run
+# the other functions and print (or convert to json later)
+
+require("./program_declarations.rb")
 require "pp"
 require "json"
 require "./ast.rb"
+require "./remove_decls.rb"
+
+ls_file = ARGV[0]
+ast = `lsc --ast --json #{ls_file}`	
+if ast == ""
+	exit()
+end
+
+def get_function_type(funcData)
+	str = ""
+	funcData.args.each do |arg|
+		str += "#{arg}->"
+	end
+	str += funcData.return_type
+	return str
+end
+
+def getDeclared_function_type(funcData, retType)
+	str = ""
+	funcData.args.each do |arg|
+		str += "#{arg.type}->"
+	end
+	str += retType
+	return str
+end
+
+def fill_inferred_types_in_references(var_references, inferred_locals, inferred_globs, inferred_funcs, funcs_dict)
+	
+	# Will map the arbitrary T' types to their correct inferred type
+	fix_hash = Hash.new
+
+	var_references.each do |ref|
+		# puts "#{ref.name}, #{ref.line_found}, #{ref.kind}"
+		scopeno = ref.scope
+		if ref.kind == "func"
+			inferred_funcs.each_pair do |funcName, func|
+				if ref.name == funcName
+					check_func = funcs_dict[funcName]
+						if check_func != nil
+							check_func.args.each_with_index do |argData, ind|
+								if isArbitraryType(argData.type)
+									fix_hash[argData.type] = func.args[ind]
+								end
+							end
+						end
+					ref.inferred_type = get_function_type(func)
+					break
+				end
+			end
+		elsif ref.kind == "global"
+			inferred_globs.each do |global_var|
+				if ref.name == global_var.name
+					if isArbitraryType(ref.declared_type)
+						fix_hash[ref.declared_type] = global_var.inferred_type
+					end
+					ref.inferred_type = global_var.inferred_type
+					break
+				end
+			end
+		else # regular local variable, might be reference to global or func though.
+			found = false
+			inferred_locals.each do |local_var|
+				# puts "local #{local_var.name}, #{local_var.scope}"
+				if ref.name == local_var.name and ref.scope == local_var.scope
+					if isArbitraryType(ref.declared_type)
+						fix_hash[ref.declared_type] = local_var.inferred_type
+					end
+					ref.inferred_type = local_var.inferred_type
+					found = true
+					break
+				end
+			end
+			if not found
+				inferred_globs.each do |global_var|
+					# puts "global #{global_var.name}"
+					if ref.name == global_var.name and ref.kind == "global"
+						if isArbitraryType(ref.declared_type)
+							fix_hash[ref.declared_type] = global_var.inferred_type
+						end
+						ref.inferred_type = global_var.inferred_type
+						ref.kind = "global"
+						found = true
+						break
+					end
+				end
+			elsif not found
+				inferred_funcs.each_pair do |funcName, func|
+					if ref.name == funcName
+						ref.inferred_type = get_function_type(func)
+						check_func = funcs_dict[funcName]
+						if check_func != nil
+							check_func.args.each_with_index do |argData, ind|
+								if isArbitraryType(argData.type)
+									fix_hash[argData.type] = func.args[ind]
+								end
+							end
+						end
+						ref.kind = "func"
+						found = true
+						break
+					end
+				end
+			end
+		end
+	end
+
+	funcs_dict.each_pair do |name1, funcData|
+		inferred_funcs.each_pair do |name2, func|
+			if name1 == name2
+				# puts "match #{name1}"
+				check_func = funcData
+				check_func.args.each_with_index do |argData, ind|
+					# puts "type is #{argData.type}"
+					if isArbitraryType(argData.type)
+						# puts "arbitrary #{argData.type}"
+						fix_hash[argData.type] = func.args[ind]
+					end
+				end
+				if isArbitraryType(check_func.return_type)
+					fix_hash[check_func.return_type] = func.return_type
+					funcData.return_type = func.return_type
+				end
+			end
+		end
+	end
+end
+
+def fill_types_from_references(local_vars, global_vars, var_references)
+	var_references.each do |varRef|
+		if local_vars[varRef.func_name] != nil
+			local_vars[varRef.func_name].each_pair do |name, data|
+				if varRef.name == name and isArbitraryType(data.declared_type)
+					local_vars[varRef.func_name][name].declared_type = varRef.declared_type
+					break
+				end
+			end
+		end
+		global_vars.each_pair do |name, data|
+			if varRef.name == name and isArbitraryType(data.declared_type)
+				global_vars[name].declared_type = varRef.declared_type
+				break
+			end
+		end
+	end
+end
 
 if ARGV.size == 0
 	puts("File name missing")
@@ -14,12 +163,39 @@ if ARGV[0].split('.')[-1] != 'ls'
 end
 
 ls_file = ARGV[0]
-ast = `lsc --ast --json #{ls_file}`	
+
+f_ls_program = File.open(ls_file, "r")
+ls_program = f_ls_program.read
+f_ls_program.close()
+
+ls_decl_free = remove_decls(ls_program)
+
+f_decl_free = File.open("decls_free.ls", "w")
+f_decl_free.write(ls_decl_free)
+f_decl_free.close()
+
+ast = `lsc --ast --json decls_free.ls`	
 if ast == ""
 	exit()
 end
+File.delete("decls_free.ls")
 
-# puts ast
+res_declared = getProgramDeclarationsAndReferences(ls_program)
+declared_funcs  = res_declared[0]
+declared_globs  = res_declared[1]
+declared_locals = res_declared[2]
+res_references = res_declared[3]
+res_bad_assigns = res_declared[4]
+
+res_var_infers = parse_locals_globals_infers(ls_program)
+inferred_locals = res_var_infers[0]
+inferred_globs  = res_var_infers[1]
+inferred_funcs  = parse_function_infers(ls_program)
+
+fill_types_from_references(declared_locals, declared_globs, res_references)
+fill_inferred_types_in_references(res_references, inferred_locals, inferred_globs, inferred_funcs, declared_funcs)
+
+completionHash = try_to_complete_missing_types(inferred_locals, inferred_globs, inferred_funcs, declared_locals, declared_globs, declared_funcs, res_references)
 
 ast_j = JSON.parse(ast)
 
@@ -61,11 +237,46 @@ ast_j['lines'].each {|line|
 		changeIndexGetToMethodGet(line)
 	end
 }
-# pp ast_j
+
 ast = Ast.new ast_j
 
+res_bad_assigns.each_pair do |lineno, bA|
+	puts "Bad Assign at line #{bA.lineno+1} #{bA.line} (#{bA.var1} :- #{bA.type1}, #{bA.var2} :- #{bA.type2})"
+end
+
+# ---------- In this line the declaration results are added -----
+ast.add_completion_subtype_equations(completionHash)
+# ---------------------------------------------------------------
+
+f = StringIO.new
+$stdout = f
+
 ast.get_vars
+
 puts "--------------------------------------------------------------------------------------------------------------"
 # pp ast
 puts "--------------------------------------------------------------------------------------------------------------"
 
+ls_program_inferred = $stdout.string
+
+res_var_infers = parse_locals_globals_infers(ls_program_inferred, true)
+inferred_locals = res_var_infers[0]
+inferred_globs  = res_var_infers[1]
+inferred_funcs  = parse_function_infers(ls_program_inferred, true)
+fill_inferred_types_in_references(res_references, inferred_locals, inferred_globs, inferred_funcs, declared_funcs)
+
+puts "Variable References:"
+res_references.each do |data|
+	if data.declared_type == nil
+		data.declared_type = "func"
+	end
+	if isArbitraryType(data.declared_type)
+		data.declared_type = "NIL"
+	end
+	if data.inferred_type == ""
+		data.inferred_type = "?"
+	end
+	puts "\tline #{data.line_found+1} column #{data.column_found}: \"#{data.name}\", #{data.kind} [declared: line #{data.line_declared+1}, as #{data.declared_type}], type #{data.inferred_type}"
+end
+
+STDOUT.puts $stdout.string
